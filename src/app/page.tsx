55908bf2c5b1da9547d1c04e38adc7b4c19d9f38
AAYUS KARKI <alexkarki2060@gmail.com>
2024-04-13 15:57:35 +0545

    daisy ui setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home (){
 <>  
 <div className="flex h-screen justify-center items-center bg-slate-200">
 <h1 className="text-3xl text-center text-black ">WELCOME TO THE BLOG APP BY AAYUS KARKI</h1>
+<button className="btn btn-primary">Hello daisyUI!</button>
 </div>
 </>
   )
